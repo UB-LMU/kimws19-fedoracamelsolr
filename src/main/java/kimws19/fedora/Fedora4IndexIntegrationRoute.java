b4b9b818bd02c10bb4e12b2b669a42a0f3e69b75
Ralf Claussnitzer <ralf.claussnitzer@slub-dresden.de>
2019-04-02 08:11:42 +0200

    Step 2 - Filtering events

--- src/main/java/kimws19/fedora/Fedora4IndexIntegrationRoute.java
+++ src/main/java/kimws19/fedora/Fedora4IndexIntegrationRoute.java
@@ -11,6 +11,8 @@ import static org.apache.camel.component.jms.JmsComponent.jmsComponentAutoAcknow
 
 public class Fedora4IndexIntegrationRoute extends RouteBuilder {
 
+    private static final String RESOURCE = "http://fedora.info/definitions/v4/repository#Resource";
+
     @Override
     public void configure() {
         // Obtain Camel context to modify it (usually done in configuration)
@@ -26,6 +28,11 @@ public class Fedora4IndexIntegrationRoute extends RouteBuilder {
                 .setProperty("fedoraEvents", jsonpath("wasGeneratedBy.type", List.class))
                 .setProperty("fedoraUri", jsonpath("id", String.class))
                 .setProperty("fedoraType", jsonpath("type", List.class))
+
+                // Filter out unwanted events
+                .filter(header("org.fcrepo.jms.identifier").startsWith("/"))
+                .filter(exchangeProperty("fedoraType").contains(RESOURCE))
+
                 .log("${body}");
     }
 }
