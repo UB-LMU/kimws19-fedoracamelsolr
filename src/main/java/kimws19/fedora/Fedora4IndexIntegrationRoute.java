b5aa68cd751b84fab186393f69ddb5b00e15867d
Ralf Claussnitzer <ralf.claussnitzer@slub-dresden.de>
2019-04-02 08:22:48 +0200

    Step 4 - Obtaining a Fedora resource representation

--- src/main/java/kimws19/fedora/Fedora4IndexIntegrationRoute.java
+++ src/main/java/kimws19/fedora/Fedora4IndexIntegrationRoute.java
@@ -2,12 +2,15 @@ package kimws19.fedora;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
 import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.http.HttpHeaders;
 
 import javax.jms.ConnectionFactory;
 import java.util.List;
 
+import static org.apache.camel.component.http4.HttpMethods.GET;
 import static org.apache.camel.component.jms.JmsComponent.jmsComponentAutoAcknowledge;
 
 public class Fedora4IndexIntegrationRoute extends RouteBuilder {
@@ -42,9 +45,18 @@ public class Fedora4IndexIntegrationRoute extends RouteBuilder {
                 .otherwise()
                     .log(LoggingLevel.ERROR, "Unexpected events: ${exchangeProperty.fedoraEvents}");
 
+        // Route for obtaining a Fedora resource in RDF-XML format
+        from("direct:fcrepo-resource").id("fcrepo-get-resource")
+                .setHeader(HttpHeaders.ACCEPT, constant("application/rdf+xml"))
+                .setHeader(Exchange.HTTP_URI, exchangeProperty("fedoraUri"))
+                .setHeader(Exchange.HTTP_METHOD, GET) // default would be post
+                .to("http4://fedora?authUsername=fedoraAdmin&authPassword=secret3")
+                .log("${body}");
+
         // Route to handle resource modifications
         from("direct:modified").id("handle-modifications")
-                .log("Resource ${exchangeProperty.fedoraUri} was modified");
+                .log("Resource ${exchangeProperty.fedoraUri} was modified")
+                .to("direct:fcrepo-resource");
 
     }
 }
